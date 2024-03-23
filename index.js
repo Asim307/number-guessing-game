8053642b867ae446ed03417296fb03673f10aa09
Asim307 <asimshaikhby0097@gmail.com>
2024-03-23 14:10:42 -0700

    pushing my number-guess-game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "guess a random number between 1-6 in your mind and type here",
+    }
+]);
+if (answer.userguessednumber === randomnumber) {
+    console.log("congrats you win! you guessed right number");
+}
+else {
+    console.log("you lost! you guessed wrong number");
+}
